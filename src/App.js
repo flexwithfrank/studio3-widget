392f82f41300a0c9cbaa99b7590717995dcd31a7
videovulture <30363299+videovulture@users.noreply.github.com>
2022-08-13 11:20:24 -0700

    main-2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Container } from '@mui/material';
 function App() {
   return (
     <Container maxWidth="xl">
-    <div style={{width: "100%", margin: 2}} className="App">
+    <div style={{maxWidth: "100%"}} className="App">
       <header className="App-header">
        <script src="https://widgets.mindbodyonline.com/javascripts/healcode.js" type="text/javascript"></script>
         <healcode-widget data-type="schedules" data-widget-partner="object" data-widget-id="0f179021470c" data-widget-version="1" >
