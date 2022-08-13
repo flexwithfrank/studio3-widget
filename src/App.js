8f48940c388f32b8ea8081a57a94f90f0cc308ef
videovulture <30363299+videovulture@users.noreply.github.com>
2022-08-13 11:27:36 -0700

    main-4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import { Container } from '@mui/material';
 
+const styles = {
+  position: "relative"
+}
 
 function App() {
   return (
-    <Container maxWidth="xl">
-    <div style={{maxWidth: "100%"}} className="App">
-      <header className="App-header">
-       <script src="https://widgets.mindbodyonline.com/javascripts/healcode.js" type="text/javascript"></script>
+    <Container disableGutters="true" maxWidth="lg">
+      <div>
         <healcode-widget data-type="schedules" data-widget-partner="object" data-widget-id="0f179021470c" data-widget-version="1" >
         </healcode-widget>
-      </header>
-    </div>
+        </div>
     </Container>
   );
 }
