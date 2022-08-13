f83888a052356e526ff05fecb5d7adc00d38f379
videovulture <30363299+videovulture@users.noreply.github.com>
2022-08-13 11:19:02 -0700

    main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container } from '@mui/material';
+
 
 function App() {
   return (
-    <div style={{width: "100%", padding: 2}} className="App">
+    <Container maxWidth="xl">
+    <div style={{width: "100%", margin: 2}} className="App">
       <header className="App-header">
        <script src="https://widgets.mindbodyonline.com/javascripts/healcode.js" type="text/javascript"></script>
-
-<healcode-widget data-type="schedules" data-widget-partner="object" data-widget-id="0f179021470c" data-widget-version="1" ></healcode-widget>
+        <healcode-widget data-type="schedules" data-widget-partner="object" data-widget-id="0f179021470c" data-widget-version="1" >
+        </healcode-widget>
       </header>
     </div>
+    </Container>
   );
 }
 
