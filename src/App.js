56fe61391759d380717d1dc172799b477c5dffa7
videovulture <30363299+videovulture@users.noreply.github.com>
2022-08-15 22:32:21 -0700

    p-widget-1032

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import * as React from 'react';
-import { Container, Box, Typography, Stack, Link } from '@mui/material';
+import { Container, Box, Typography, Stack } from '@mui/material';
 import BasicModal from './Modal';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Footer from './Footer';
+import { Link } from 'react-router-dom';
 
 
 
 function App() {
+  
   const [show, setShow] = React.useState(true);
   const handleClick = () => setShow(false);
 
@@ -21,7 +23,7 @@ function App() {
           <Typography lineHeight="24px" align='center' color="#ffffff" p={3} id="modal-modal-title" variant="h6" component="h2">
             <span className='space-font-heavy'> Studio3 Fitness is Seattle's premier boutiqe fitness studio. Don't miss out on our 10 pack special &nbsp; 
             <healcode-widget data-version="0.2" data-link-class="healcode-pricing-option-text-link" data-site-id="30089" data-mb-site-id="686934" data-bw-identity-site="false" data-type="pricing-link" data-inner-html="here." data-service-id="101165" />
-           &nbsp;Offer exp 8.19 🎉 </span>
+           &nbsp;Offer exp 8.19 🎉 </span> 
           </Typography>
           <Stack direction="row" alignItems="flex-end" justifyContent="flex-end">
               <IconButton onClick={handleClick} sx={{color: "white"}} aria-label="close">
@@ -38,6 +40,7 @@ function App() {
         </healcode-widget>
         </div>
         <Footer />
+        <Link to="/purchase-landing">Link to p-widget</Link>
     </Container>
 
   );
---
+++ src/purchase-landing.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { Stack, Box, Typography, Modal, Backdrop } from '@mui/material';
+import ButtonV2 from './ButtonV2';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  borderRadius: 3,
+  transition: "5s ease-in-out"
+};
+
+const PurchaseLanding = () => {
+    const [show, setShow] = React.useState(true);
+    const handleClose = () => setShow(false);
+
+
+  return (
+    <div>
+        <Modal
+        open={show}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        disableAutoFocus="true" 
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}>
+            
+        <Box sx={style}>
+            <div>
+                {/* full width image */}
+            <img style={{borderRadius: "10px 10px 0 0"}} src="https://res.cloudinary.com/djjb1vyjm/image/upload/v1660541084/1_uxhi8p.png" width="100%" alt="" />
+            </div>
+            <Box p={2}>
+                {/* title */}
+            <Typography align="center" id="modal-modal-title" variant="h5" component="h2">
+            <span className='space-font-heavy'>Buy your [10-pack]</span>
+            </Typography>
+
+            {/*  onboarding message */}
+            <Stack pb={3} alignItems="center" justifyContent="center" direction="row" spacing={2}>
+                <Typography px={1} align="center" id="modal-modal-description" sx={{ mt: 2 }}>
+                    <span className='space-font-light'>Once completed download the Mindbody app to add, update, and book your workouts!</span>
+                </Typography>
+            </Stack>
+
+            {/* button group left/right */}
+            
+            <Stack direction="row" justifyContent="center" alignItems="center">
+              <ButtonV2 click={handleClose} cta="Purchase now" />
+            </Stack>
+
+            </Box>
+        </Box>
+        </Modal>
+
+    </div>
+  );
+}
+
+
+export default PurchaseLanding
