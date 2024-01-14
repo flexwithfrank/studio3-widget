b631216b928fec4f08d9752173ecdaddc9e87b8f
videovulture <30363299+videovulture@users.noreply.github.com>
2024-01-14 07:52:26 -0800

    nav-update-2

--- src/Nav.js
+++ src/Nav.js
@@ -10,8 +10,8 @@ const Nav = () => {
     <div>
       <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://studio3-seattle.framer.website/welcome-to-studio3" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img style={styles} target="_blank" rel="noopener noreferrer"
+          <a href="https://studio3-seattle.framer.website/welcome-to-studio3" class="flex items-center space-x-3 rtl:space-x-reverse" target="_blank" rel="noreferrer">
+            <img style={styles}
               src="https://framerusercontent.com/images/tKc8viqJIGng2lVPhduGRVesrM.svg"
               class="h-2"
               alt="Studio3 Logo"
