4896c99979530a9883040cf5bd6222c864a1b612
videovulture <30363299+videovulture@users.noreply.github.com>
2024-01-13 17:46:10 -0800

    main bg

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ function App() {
 
   return (
 
-    <Container sx={{backgroundColor: "#000000"}} disableGutters="true" maxWidth="xl">
+    <Container sx={{backgroundColor: "#000000"}} disableGutters="true" maxWidth="xl" className='container-height'>
       <Container style={{display: show ? "" : "none"}} maxWidth="xl" disableGutters="true" className="gradient">
 
         <Stack py={2} direction={{xs:"column", sm: "row"}} justifyContent="center" alignItems="center">
           <Typography lineHeight="24px" align='center' color="#ffffff" p={3} id="modal-modal-title" variant="h6" component="h2">
-            <span className='space-font-heavy'> Studio3 Fitness is Seattle's premier boutiqe fitness studio. Don't miss out on our 10 for $10 special!&nbsp; 🎉</span>
+            <span className='space-font-heavy'> Studio3 Fitness is Seattle's premier boutique fitness studio. Book your first workout today! 🎉</span>
           </Typography>
-          <ButtonV2 />
+     
           <Stack direction="row" alignItems="flex-end" justifyContent="flex-end">
               <IconButton onClick={handleClick} sx={{color: "white"}} aria-label="close">
               <CloseIcon  />
@@ -35,8 +35,7 @@ function App() {
         </Stack>
 
       </Container>
-   
-      <BasicModal />
+  
       
       <div style={{backgroundColor: "#000000"}}>
         <healcode-widget data-type="schedules" data-widget-partner="object" data-widget-id="0f179021470c" data-widget-version="1" >
